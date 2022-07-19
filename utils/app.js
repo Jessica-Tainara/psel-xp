84fb4bfe084444d61f48c0ea0de8d9afaefc7757
jessica <jessicathaynnara@gmail.com>
2022-07-19 19:56:41 -0300

    Feat: colocando rotas e middleware de erros no app

--- utils/app.js
+++ utils/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+require('express-async-errors');
+const routers = require('./router');
+const middlewareError = require('../middleware/middleware.error')
 const app = express();
 
 app.use(express.json());
-
+app.use(routers);
+app.use(middlewareError);
 module.exports = app;
