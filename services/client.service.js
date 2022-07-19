f2e208c3b16cd84bfb6cd2f8d8a805faecc42415
jessica <jessicathaynnara@gmail.com>
2022-07-19 19:55:38 -0300

    Feat: metodos balance e history client service

--- services/client.service.js
+++ services/client.service.js
@@ -14,17 +14,26 @@ const deposit = async ({ conta: accountId, Valor}) => {
 
 const withdraw = async ({ codConta: accountId, Valor}) => {
 
-    const account = await Account.findByPk(accountId);
+  const account = await Account.findByPk(accountId);
   
-    const balance = Number(account.balance);
+  const balance = Number(account.balance);
   
-    if(balance < Valor) throw customError(200, "Saldo insuficiente")
+  if(balance < Valor) throw customError(200, "Saldo insuficiente")
   
-    await Account.update({ balance: balance - Valor }, { where: { id: accountId },});
+  await Account.update({ balance: balance - Valor }, { where: { id: accountId },});
   
-    await History.create({ accountId, transaction: 'Saque', value: Valor, details:'' });
+  await History.create({ accountId, transaction: 'Saque', value: Valor, details:'' });
   
     return { message: 'Saque finalizado com sucesso!' }
-  };
+};
   
+const balance = async (conta) => {
+  const { balance: saldo, clientId: codCliente } = await Account.findByPk(conta);
+    return { codCliente, conta, saldo };
+};
+  
+const history = async (accountId) => {
+  const history = await History.findAll({ where: { accountId } });
+    return history;
+};
 module.exports = { deposit, withdraw, balance, history }; 
