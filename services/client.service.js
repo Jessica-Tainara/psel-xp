de4dfde3096c44ae08bc686b07384828418ca020
jessica <jessicathaynnara@gmail.com>
2022-07-19 19:53:37 -0300

    Feat: metodo withdraw service Client

--- services/client.service.js
+++ services/client.service.js
@@ -12,4 +12,19 @@ const deposit = async ({ conta: accountId, Valor}) => {
   return { message: 'Depósito finalizado com sucesso!' }
 };
 
+const withdraw = async ({ codConta: accountId, Valor}) => {
+
+    const account = await Account.findByPk(accountId);
+  
+    const balance = Number(account.balance);
+  
+    if(balance < Valor) throw customError(200, "Saldo insuficiente")
+  
+    await Account.update({ balance: balance - Valor }, { where: { id: accountId },});
+  
+    await History.create({ accountId, transaction: 'Saque', value: Valor, details:'' });
+  
+    return { message: 'Saque finalizado com sucesso!' }
+  };
+  
 module.exports = { deposit, withdraw, balance, history }; 
