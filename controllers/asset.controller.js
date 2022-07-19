703382cdad0fdf7a90d978deba5a01d8bb45817b
jessica <jessicathaynnara@gmail.com>
2022-07-19 19:32:39 -0300

    Feat: asset.controller endoint POST ativos/comprar

---
+++ controllers/asset.controller.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const serviceAsset = require('../services/asset.service')
+const validateAsset = require('../middleware/validate.buying-assets')
+
+const assetRouter = express.Router();
+
+assetRouter.post('/comprar', validateAsset, async (req, res) => {
+    const response = await serviceAsset.buy(req.body);
+    return res.status(201).json(response);
+});
+
+module.exports = assetRouter;
