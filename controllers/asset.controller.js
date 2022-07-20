038f831d45dd3d293702da258cc7d4fec4245285
jessica <jessicathaynnara@gmail.com>
2022-07-20 17:29:42 -0300

    feat: middleware de autenticaçao nas rotas

--- controllers/asset.controller.js
+++ controllers/asset.controller.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const serviceAsset = require('../services/asset.service')
 const validateAsset = require('../middleware/validate.buying-assets')
+const authMiddleware = require('../middleware/middleware.auth.token');
 
 const assetRouter = express.Router();
 
-assetRouter.post('/comprar', validateAsset, async (req, res) => {
-    const response = await serviceAsset.buy(req.body);
-    return res.status(201).json(response);
+assetRouter.post('/comprar', authMiddleware, validateAsset, async (req, res) => {
+  const response = await serviceAsset.buy(req.body, res.locals.payload);
+  return res.status(201).json(response);
 });
 
-assetRouter.post('/vender', validateAsset,  async (req, res) => {
-    const response = await serviceAsset.sell(req.body);
-    return res.status(201).json(response);
+assetRouter.post('/vender', authMiddleware, validateAsset,  async (req, res) => {
+  const response = await serviceAsset.sell(req.body, res.locals.payload);
+  return res.status(201).json(response);
 });
 
+assetRouter.get('/', async (req, res) => {
+  const response = await serviceAsset.getAll();
+  return res.status(200).json(response);
+});
 
 assetRouter.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    const response = await serviceAsset.getById(id);
-    return res.status(200).json(response);
+  const { id } = req.params;
+  const response = await serviceAsset.getById(id);
+  return res.status(200).json(response);
 });
 
-assetRouter.get('/conta/:id', async (req, res) => {
-    const { id } = req.params;
-    console.log(id)
-    const response = await serviceAsset.getByClient(id);
-    return res.status(200).json(response);
+assetRouter.get('/conta/:id', authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const response = await serviceAsset.getByClient(id, res.locals.payload);
+  return res.status(200).json(response);
 });
 
 module.exports = assetRouter;
--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,34 +1,30 @@
 const express = require('express');
 const serviceClient = require('../services/client.service');
 const validateSaqueDeposito = require('../middleware/validate.deposit-saque');
+const authMiddleware = require('../middleware/middleware.auth.token');
 
 const clientRouter = express.Router();
 
 clientRouter.post('/deposito', validateSaqueDeposito, async (req, res) => {
-    const response = await serviceClient.deposit(req.body);
-    return res.status(201).json(response);
+  const response = await serviceClient.deposit(req.body);
+  return res.status(201).json(response);
 });
 
-clientRouter.post('/saque', validateSaqueDeposito, async (req, res) => {
-    const response = await serviceClient.withdraw(req.body);
-    return res.status(201).json(response);
+clientRouter.post('/saque', authMiddleware, validateSaqueDeposito, async (req, res) => {
+  const response = await serviceClient.withdraw(req.body, res.locals.payload);
+  return res.status(201).json(response);
 });
 
-clientRouter.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    const response = await serviceClient.balance(id);
-    return res.status(200).json(response);
+clientRouter.get('/:id', authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const response = await serviceClient.balance(id, res.locals.payload);
+  return res.status(200).json(response);
 });
 
-clientRouter.get('/historico/:id', async (req, res) => {
-    const { id } = req.params;
-    const response = await serviceClient.history(id);
-    return res.status(200).json(response);
-});
-
-clientRouter.post('/', async (req, res) => {
-    const response = await serviceClient.register(req.body);
-    return res.status(201).json(response);
+clientRouter.get('/historico/:id', authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const response = await serviceClient.history(id, res.locals.payload);
+  return res.status(200).json(response);
 });
 
 module.exports = clientRouter;
