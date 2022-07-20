22373f3f9cb3a908e9dc1a3e270ece8faa3e56ec
jessica <jessicathaynnara@gmail.com>
2022-07-20 17:35:22 -0300

    refactor: middlewares compra e venda , saque e dep

--- middleware/validate.buying-assets.js
+++ middleware/validate.buying-assets.js
@@ -1,23 +1,27 @@
-module.exports = (req, res, next) => {
+const customError = require("../utils/customError");
+
+module.exports = (req, _res, next) => {
   const { codCliente = '', codAtivo = '', qtdeAtivo = '' } = req.body;
 
   const createErrors = (key) => [
-    [400, `"${key}" is required`],
-    [422, `"${key}" must be a number`],
-    [422, `"${key}" must be greater than or equal to 1`]];
+    [400, `"${key}" é necessário`],
+    [422, `"${key}" deve ser um número`],
+    [422, `"${key}" deve ser um inteiro maior ou igual a 1`]];
     
   const createChecks = (key) => 
     [!key, typeof key !== 'number', (!Number.isSafeInteger(key) || key < 1)];
 
   const keys = ['codCliente', 'codAtivo', 'qtdeAtivo']
     
-  const index = [createChecks(codCliente),
+  const indexArray = [createChecks(codCliente),
     createChecks(codAtivo),createChecks(qtdeAtivo)].map((check, i)=>{
       return check.includes(true) ? [i, check.indexOf(true)] : undefined})
       .find((erro)=>erro);
 
-  const error = index && res.status(createErrors(keys[index[0]])[index[1]][0])
-    .json({ message: createErrors(keys[index[0]])[index[1]][1]});
-  
-    return error || next();
+  if(indexArray){
+    const [indexKey, indexError] = indexArray;
+    const [status, message] = createErrors(keys[indexKey])[indexError];
+    throw customError(status, message);
+  }
+    return next();
   };
--- middleware/validate.deposit-saque.js
+++ middleware/validate.deposit-saque.js
@@ -1,14 +1,14 @@
-const customError = require('../error/customError');
+const customError = require('../utils/customError');
 
 module.exports = (req, _res, next) => {
   const { codCliente = '', valor = '' } = req.body;
   const errors = [
-    [400, `"codCliente" is required`],
-    [422, `"codCliente" must be a number`],
-    [422, `"codCliente" must be greater than or equal to 1`],
-    [400, `"valor" is required`],
-    [422, `"valor" must be a number`],
-    [422, `"valor" must be greater than or equal to 1`]];
+    [400, '"codCliente" é necessário'],
+    [422, '"codCliente" deve ser um número'],
+    [422, '"codCliente" deve ser um inteiro maior ou igual a 1'],
+    [400, '"valor" é necessário'],
+    [422, '"valor" deve ser um número'],
+    [422, '"valor" deve ser um inteiro maior ou igual a 1']];
   
   const index = [codCliente === '', typeof codCliente !== 'number', 
     (!Number.isSafeInteger(codCliente) || codCliente < 1), 
