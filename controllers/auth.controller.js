2efea70a0d0a316ce6b43322b8b896b47a716410
jessica <jessicathaynnara@gmail.com>
2022-07-20 17:23:59 -0300

    feat: authController login e cadastro

---
+++ controllers/auth.controller.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const serviceClient = require('../services/client.service');
+const serviceAuth = require('../services/auth.service');
+const validateRegister = require('../middleware/middleware.register.client');
+
+const authController = express.Router();
+
+authController.post('/registrar', validateRegister, async (req, res) => {
+  const response = await serviceClient.register(req.body);
+  return res.status(201).json(response);
+});
+
+authController.post('/entrar', async (req, res) => {
+  const token = await serviceAuth.authentication(req.body);
+  return res.status(201).json({ token });
+});
+
+module.exports = authController;
