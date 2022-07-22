d622b4374ebf1f309691bb2b0976d89b2ead1db1
jessica <jessicathaynnara@gmail.com>
2022-07-22 01:15:40 -0300

    feat: teste asset service metodo sell

--- tests/services/assets.service.test.js
+++ tests/services/assets.service.test.js
@@ -138,3 +138,82 @@ describe('1 - Ao realizar uma compra de ativos:', async () => {
   })
 });
 
+describe('2 - Ao realizar uma venda de ativos:', async () => {
+  before(async () => {
+    sinon.stub(Account, "findOne").resolves({ balance: 3000.00, id: 1 });
+    sinon.stub(Client, "findOne").resolves({ id: 1 })
+    sinon.stub(BuyingAsset, "findOne").resolves({ qtd: 5 });
+    sinon.stub(Account, "update").resolves();
+    sinon.stub(History, "create").resolves()
+    sinon.stub(BuyingAsset, "update").resolves();
+    const findByPk = sinon.stub(Asset, 'findByPk');
+    findByPk.withArgs(1).returns({ price: 70.00, qtd: 100 });
+  })
+
+  after(async () => {
+    Account.findOne.restore();
+    Client.findOne.restore();
+    BuyingAsset.findOne.restore();
+    BuyingAsset.update.restore();
+    Account.update.restore();
+    History.create.restore();
+    Asset.findByPk.restore();
+  })
+
+  it('Retorna umma mensagem de sucesso ao ser concluída', async () => {
+    const response = await sell({
+      codCliente: 1,
+      codAtivo: 1,
+      qtdeAtivo: 5
+    }, { email: 'teste@test.com' });
+
+    expect(response.message).to.equal('Venda finalizada com sucesso!');
+
+  })
+  it('Retorna um erro com a mensagem "Não autorizado!" e status 401 quando o usuário não for autorizado', async () => {
+    let err;
+    try {
+      const response = await sell({
+        codCliente: 2,
+        codAtivo: 1,
+        qtdeAtivo: 105
+      }, {email: 'teste@test.com'});
+    } catch (e) {
+      err = e
+    }
+    expect(err).to.exist;
+    expect(err.message).to.equal('Não autorizado!');
+  })
+
+  it('Retorna um erro com a quantidade de ativos disponivel na carteira ao tentar vender quantidade maior que a disponível ', async () => {
+    let err;
+    try {
+      const response = await sell({
+        codCliente: 1,
+        codAtivo: 1,
+        qtdeAtivo: 105
+      }, {email: 'teste@test.com'});
+    } catch (e) {
+      err = e
+    }
+    expect(err).to.exist;
+    expect(err.message).to.equal('Quantidade disponível desse ativo para venda: 5');
+  })
+  it('Retorna mensagem de erro caso ocorra alguma falha na transação', async () => {
+    History.create.restore();
+    sinon.stub(History, "create").throws(new Error(''));
+    let err;
+    try {
+      const response = await sell({
+        codCliente: 1,
+        codAtivo: 1,
+        qtdeAtivo: 1
+      }, {email: 'teste@test.com'});
+    } catch (e) {
+      err = e
+    }
+    expect(err).to.exist;
+    expect(err.message).to.equal('Não foi possível finalizar a transação');
+  })
+});
+
