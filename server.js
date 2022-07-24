f436fcd29540f2e6d5066275066a4f159fa47107
jessica <jessicathaynnara@gmail.com>
2022-07-24 04:34:51 -0300

    swagger

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const swaggerConfig = require('./docs/swagger.config')
 
 const port = process.env.PORT || 3000;
 
-const swaggerDoc = swaggerJSDoc(swaggerConfig)
-app.use('/docs',swaggerUI.serve, swaggerUI.setup(swaggerDoc) )
+// const swaggerDoc = swaggerJSDoc(swaggerConfig)
+// app.use('/docs',swaggerUI.serve, swaggerUI.setup(swaggerDoc) )
 
 
 app.listen(port, () => console.log('Ouvindo na porta', port));
